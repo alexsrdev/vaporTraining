673dffab5e3aaea84ec6dbd875dd211a0c84b5a7
Alex <dice_el_piano@hotmail.com>
2016-09-21 13:41:27 +0000

    First commit, but not new template.

---
+++ Sources/App/main.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
+    return try drop.view.make("hello")
+}
+
+/* drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
+    return try drop.view.make("welcome", [
+    	"message": Node.string(drop.localization[lang, "welcome", "title"])
+    ])
+} */
+
+//drop.resource("posts", PostController())
+
+drop.run()
